5703688f3a55cb0522ae3771e8810c08e6209654
Jake Yesbeck <jake@apartmentlist.com>
2016-01-30 16:06:41 -0800

    Simplify Requires

--- lib/requires.rb
+++ lib/requires.rb
@@ -1,4 +1,8 @@
 # master require file for whole tree
 require 'faraday'
 
-require 'requires/models'
+['../../models'].each do |dir|
+  Dir[File.expand_path("#{dir}/*.rb", __FILE__)].each do |file|
+    require file
+  end
+end
