597ff3154d8c1623a103a98bb0cf35a08de9687c
Jake Yesbeck <jake@apartmentlist.com>
2016-02-11 21:37:49 -0800

    Utilization of Session class in address/password authenticator

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -36,7 +36,11 @@ module HonestRenter
     end
 
     def build_session
-      raw_session = HonestRenter::Post.new('members/session', address: address, password: password).call
+      raw_session = HonestRenter::Post.new('members/session', address: @address, password: @password).call
+      HonestRenter::Session.new(
+        raw_session.headers['honr-authentication-token'],
+        raw_session.headers['honr-session']
+      )
     end
   end
 end
--- lib/session.rb
+++ lib/session.rb
@@ -1,10 +1,10 @@
 module HonestRenter
   class Session
-    attr_reader :honr_session, :honr_authentiation_token
+    attr_reader :honr_authentiation_token, :honr_session
 
-    def initialize(honr_session, honr_authentiation_token)
-      @honr_session = honr_session
+    def initialize(honr_authentiation_token, honr_session)
       @honr_authentiation_token = honr_authentiation_token
+      @honr_session = honr_session
     end
   end
 end
