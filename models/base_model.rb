96b7f5b0b1763cdf1e6ca935f38fe852a3b9e198
Jake Yesbeck <jake@apartmentlist.com>
2016-02-18 23:58:27 -0800

    Mapper in BaseModel for camelCase to snake_case attributes

--- models/base_model.rb
+++ models/base_model.rb
@@ -2,10 +2,17 @@ class BaseModel
 
   def initialize(data)
     data.each do |key, value|
-      if self.respond_to?(key)
-        send(:"#{key}=", value)
+      snaked = snake_case(key)
+      if self.respond_to?(snaked)
+        send(:"#{snaked}=", value)
       end
     end
+  end
+
+  private
+
+  def snake_case(key)
+    key.gsub(/([a-z])([A-Z])/, '\1_\2').downcase
   end
 
   class << self
