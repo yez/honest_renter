8b12940bad4cb336e6151539c93e65a40e7dd623
Jake Yesbeck <jake@apartmentlist.com>
2016-02-18 23:50:20 -0800

    Remove secret key auth

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -1,8 +1,8 @@
 module HonestRenter
   class Authenticator
     class << self
-      def from_secret_key_member_id(secret_key, member_id)
-        SecretKeyMemberIdAuthenticator.new(secret_key, member_id)
+      def from_secret_key_member_id(*_)
+        raise 'secret key auth is not supported at this time'
       end
 
       def from_address_and_password(address, password)
@@ -16,37 +16,6 @@ module HonestRenter
 
     def session
       build_session
-    end
-  end
-
-  class SecretKeyMemberIdAuthenticator < Authenticator
-    require 'openssl'
-
-    ONE_HOUR = 3600
-
-    def after_initialize(secret_key, member_id)
-      @secret_key = secret_key
-      @member_id = member_id
-    end
-
-    def build_session
-      json_hash = JSON(raw_hash)
-      digest = OpenSSL::Digest.new('sha256')
-      encoded = OpenSSL::HMAC.hexdigest(digest, @secret_key, json_hash)
-
-      HonestRenter::Session.new(encoded, json_hash)
-    end
-
-    def raw_hash
-      now = Time.now
-
-      {
-        apiKey: ENV['HONEST_RENTER_API_KEY'],
-        authorization: 'member',
-        expires: now.to_i + ONE_HOUR,
-        person: @member_id,
-        renewableUntil: now.to_i + (3 * ONE_HOUR)
-      }
     end
   end
 
