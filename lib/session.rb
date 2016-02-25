e204dca862e9bc4f4e4ee3fc481f8adcfc5dcb65
Jake Yesbeck <jake@apartmentlist.com>
2016-02-24 17:59:14 -0800

    Add expires_at and expired? to session

--- lib/session.rb
+++ lib/session.rb
@@ -4,7 +4,17 @@ module HonestRenter
 
     def initialize(honr_authentication_token, honr_session)
       @honr_authentication_token = honr_authentication_token.to_s
-      @honr_session = honr_session
+      @honr_session = JSON.parse(honr_session)
+    end
+
+    def expires_at
+      return Time.now if @honr_session.nil?
+      Time.at(@honr_session['expires'])
+    end
+
+    def expired?
+      return true if @honr_session.nil? || !@honr_session.is_a?(Hash)
+      expires_at < Time.now
     end
   end
 end
