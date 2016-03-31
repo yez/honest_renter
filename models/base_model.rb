825bdc3605b3ca522c1702e1c7ce4022e7b82a89
Jake Yesbeck <jake@apartmentlist.com>
2016-03-30 22:14:01 -0700

    Add support for find by filter

--- models/base_model.rb
+++ models/base_model.rb
@@ -24,15 +24,30 @@ module HonestRenter
         args = [session, limit, offset].compact
         response = HonestRenter::FindAll.new(attr_name, *args).call
         raise response.error unless response.success?
-        response.body['data'].map do |data|
-          new(data)
-        end
+        initialize_all_from_response(response)
       end
 
       def find(id, session)
         response = HonestRenter::FindById.new(id, attr_name, session).call
         raise response.error unless response.success?
         new(response.body["data"])
+      end
+
+      def find_by_filters(filters)
+        request = HonestRenter::FindByFilter.new(attr_name, session)
+        filters.each { |filter| request.add_filter(filter) }
+        response = request.call
+
+        raise response.error unless response.success?
+        initialize_all_from_response(response)
+      end
+
+      private
+
+      def initialize_all_from_response(response)
+        response.body['data'].map do |data|
+          new(data)
+        end
       end
     end
   end
