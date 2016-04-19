a90aa30a29d835e1aa584d0fc45f1d1895ae55dc
Jake Yesbeck <jake@apartmentlist.com>
2016-04-19 15:32:30 -0700

    Allow the `find` method for "unsupported" models

--- modules/requests_unsupported.rb
+++ modules/requests_unsupported.rb
@@ -10,10 +10,6 @@ module HonestRenter
       def find_all(*)
         raise Unsupported, "this model may not use the #{__method__} method."
       end
-
-      def find(*)
-        raise Unsupported, "this model may not use the #{__method__} method."
-      end
     end
 
     private_constant :Unsupported
